759c151140dbd28186a1b0d9844cba41c5e567f7
TONYA JENNINGS <tjennings@kcs059702-tl.hitronhub.home>
2017-07-12 15:15:15 -0400

    evenodd test no wombat

--- evenodd.rb
+++ evenodd.rb
@@ -1,8 +1,13 @@
+def letter?(lookAhead)
+	lookAhead=~/[[:alpha:]]/
+end
+def numberic?(lookAhead)
+	lookAhead =~/[[:digit:]]/
+end
 def evenoddfunction(number)
 	if number % 2 == 0 
 		"even" 
 	else
 		"odd"
 	end
-
 end
