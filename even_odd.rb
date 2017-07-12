731f3edc1c14f0d5c22292bee8e6c6aedf8237e1
TONYA JENNINGS <tjennings@kcs059702-tl.hitronhub.home>
2017-07-12 14:10:44 -0400

    even odd

---
+++ even_odd.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+require_relative "evenodd.rb"
+
+class TestEvenOdd < Minitest::TestEvenOdd
+	
+	def test_asert_that_1_equals_1r
+
+	def test_new_test_example
+		assert_equal (1, one())
+	end
+
---
+++ even_odd_test.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+require_relative "evenodd.rb"
+
+class TestEvenOdd < Minitest::Test
+	
+	def test_assert_that_1_equals_1
+		assert_equal(1, 1)
+	end
+
+	end
+	
